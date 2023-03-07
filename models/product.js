89480cb00d4978a833f87857b5a07394c3e9b4f4
Harsha-kalava <harshcode16@gmail.com>
2023-03-07 19:00:58 +0530

    added edit and delete functionalities for ecommerce website

--- models/product.js
+++ models/product.js
@@ -1,22 +1,70 @@
+const mongodb = require('mongodb')
 const getDb = require('../util/database').getDb
 
 class Product{
-  constructor(title,price,description,imageURL){
+  constructor(title,price,description,imageURL,id){
     this.title = title,
     this.price = price,
     this.description = description,
-    this.imageURL = imageURL
+    this.imageURL = imageURL,
+    this._id = id
   }
 
   save(){
     const db = getDb()
-    return db.collection('Product')
-    .insertOne(this)
+    let dbOp
+    if(this._id){
+      // update product
+      dbOp = db.collection('Product').updateOne({_id:new mongodb.ObjectId(this._id)},{$set : this})
+    }else{
+      dbOp = db.collection('Product').insertOne(this)
+    }
+    return dbOp
     .then(result=>{
       console.log(result,'created at models-->product')
     })
     .catch(err=>{
       console.log(err,'happend at models-->product')
+    })
+  }
+
+  static fetchAll(){
+    const db = getDb()
+    return db.collection('Product')
+    .find()
+    .toArray()
+    .then(products=>{
+      console.log(products)
+      return products
+    })
+    .catch(err=>{
+      console.log(err,'happened at models-->static fetch all')
+    })
+  }
+
+  static findById(prodId){
+    const db = getDb()
+    return db.collection('Product')
+    .find({_id: new mongodb.ObjectId(prodId)})
+    .next()
+    .then(item=>{
+      console.log(item,'single item at models findById')
+      return item
+    })
+    .catch(err=>{
+      console.log(err,'static findByID at models')
+    })
+  }
+
+  static deleteById(prodId){
+    const db = getDb()
+    return db.collection("Product")
+    .deleteOne({_id: new mongodb.ObjectId(prodId)})
+    .then(()=>{
+      console.log('deleted successfully')
+    })
+    .catch(err=>{
+      console.log(err)
     })
   }
 }
